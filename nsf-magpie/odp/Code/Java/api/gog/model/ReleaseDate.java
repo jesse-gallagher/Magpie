00ad642a1c723ef9a0b226ca9f8efcf91dd2aa75
Jesse Gallagher <jesse@secondfoundation.org>
2025-03-15 20:05:47 -0400

    Add exception case for some observed bad time data

--- nsf-magpie/odp/Code/Java/api/gog/model/ReleaseDate.java
+++ nsf-magpie/odp/Code/Java/api/gog/model/ReleaseDate.java
@@ -8,8 +8,14 @@ import java.time.format.DateTimeFormatter;
 
 public record ReleaseDate(String date, int timezoneType, String timezone) {
 	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.nnnnnn");
+	private static final String NULL_TIME = "-0001-11-30 00:00:00.000000";
 	
 	public OffsetDateTime toOffsetDateTime() {
+		// Some GOG games come back with an invalid date like above, so punt on them
+		if(NULL_TIME.equals(date)) {
+			return OffsetDateTime.now();
+		}
+		
 		ZoneId zoneId = ZoneId.of(timezone);
 		LocalDateTime localDateTime = LocalDateTime.from(FORMATTER.parse(date));
 		ZonedDateTime dt = ZonedDateTime.of(localDateTime, zoneId);
